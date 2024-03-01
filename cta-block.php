b5d4a91ba87a1baf283863b782a0cbf6d909b2d3
MaxGruson <max@gruson.studio>
2024-03-01 17:59:01 +0100

    Updated version number

--- cta-block.php
+++ cta-block.php
@@ -4,7 +4,7 @@
  * Description:       A "call to action" (CTA) draws special attention to an action, such as "Download the annual report here." The Call To Action block consists of a title, description and link.
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           0.1.0
+ * Version:           1.0.2
  * Author:            <a href="https://max.gruson.studio" target="_blank">Max Gruson</a>
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
