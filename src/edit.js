74465d1851b655e0fce5ee0f25004a8e21dd30fb
MaxGruson <max@gruson.studio>
2024-04-26 18:21:51 +0200

    Revert "Revert "Revert "Created heading block"""
    This reverts commit 6427d10d48411f9825e81d984e0f70d14a92b208.

--- src/edit.js
+++ src/edit.js
@@ -11,7 +11,13 @@ import { __ } from '@wordpress/i18n';
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
  */
-import {useBlockProps, RichText} from '@wordpress/block-editor';
+import {__experimentalLinkControl as LinkControl, useBlockProps, RichText, BlockControls} from '@wordpress/block-editor';
+
+import { ToolbarGroup, ToolbarButton, Popover } from '@wordpress/components';
+
+import { useState } from '@wordpress/element';
+
+import { link } from '@wordpress/icons';
 
 /**
  * The edit function describes the structure of your block in the context of the
@@ -23,8 +29,39 @@ import {useBlockProps, RichText} from '@wordpress/block-editor';
  */
 export default function Edit({attributes, setAttributes}) {
 
+	const [ showLinkPopover, setShowLinkPopover ] = useState( false );
+	const toggleLinkPopover = () => {
+			setShowLinkPopover( state => ! state );
+	};
+
 	return (
 		<>
+		{/* Toolbar zone */}
+		<BlockControls>
+			<ToolbarGroup>
+				<ToolbarButton
+					icon={link}
+					label={__('Link', 'cta-bock')}
+					onClick={toggleLinkPopover}
+					isPressed={showLinkPopover}
+				/>
+			</ToolbarGroup>
+			{showLinkPopover && (
+				<Popover>
+					<LinkControl
+						searchInputPlaceholder={__('Zoek of typ URL', 'cta-block')}
+						value={ attributes.link }
+						onChange={ ( newLink ) => {
+							setAttributes( { link: {...newLink || ''} } ) }
+						}
+						required
+					>
+					</LinkControl>
+				</Popover>
+			)}
+		</BlockControls>
+		{/* End Toolbar zone */}
+
 		{/* Main block zone */}
 		<div { ...useBlockProps() }>
 			<RichText 
@@ -32,17 +69,37 @@ export default function Edit({attributes, setAttributes}) {
 				allowedFormats={[]}
 				value={attributes.heading}
 				onChange={(heading) => setAttributes({heading})}
-				placeholder={__( 'Koptekst', 'heading-block')}
+				placeholder={__( 'Titel (bijv. Jaarverslag)', 'cta-block')}
 				required
 			/>
 			<RichText 
-				tagName='h3'
-				allowedFormats={[]}
-				value={attributes.subheading}
-				onChange={(subheading) => setAttributes({subheading})}
-				placeholder={__( 'Onderkoptekst', 'heading-block')}
+				tagName='p'
+				allowedFormats={[
+					'core/italic',
+					'core/bold',
+					'core/strikethrough',
+					'core/subscript',
+					'core/superscript',
+					'core/underline'
+				]}
+				value={attributes.body}
+				onChange={(body) => setAttributes({body})}
+				placeholder={__( 'Omschrijving (bijv. Download hier het verslag van alle activiteiten dit jaar)', 'cta-block')}
 				required
 			/>
+			<RichText 
+					tagName='div'
+					className='wp-element-button wp-block-button__link'
+					allowedFormats={[]}
+					value={() => {
+						if ( attributes.link ) {
+							attributes.link.title;
+						}
+					}}
+					onChange={(newTitle) => setAttributes({link: {...attributes.link, title: newTitle}})}
+					placeholder={__( 'Knoptekst (bijv. Downloaden)', 'cta-block')}
+					required
+				/>
 		</div>
 		{/* End Main block zone */}
 		</>
